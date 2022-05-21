23813e76f63178042c4ddd9d66b4fe1a4bc2678d
rohit <rohitluni4242@gmail.com>
2022-05-21 20:36:05 +0530

    init

--- portfolio_site_2/app.js
+++ portfolio_site_2/app.js
@@ -11,3 +11,28 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+
+var submit = document.getElementById("submit");
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+    var name = document.getElementById("name").value;
+    var mail = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    var body = "Name: "+name+"Email: "+mail+"Subject: "+subject+"Message: "+message;
+
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "rabariranchhod3@gmail.com",
+        Password : "gdonzovdfvsfryyg",
+        To : 'rabariranchhod3@gmail.com',
+        From : mail,
+        Subject : subject,
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+})
